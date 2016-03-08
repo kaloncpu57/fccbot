5aac291af6f96010c0b8bdec23d7472102384c3a
JackeL <it.davidkaiser@gmail.com>
2016-03-08 04:03:37 -0800

    Updated poll features...

--- src/dashboard/views/static/js/main.js
+++ src/dashboard/views/static/js/main.js
@@ -1,23 +1,72 @@
+//global array to store poll info....
+var pollinfo = [];
+var pollquestion = "";
+
+
 $(document).ready(function() {
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    $('.modal-trigger').leanModal();
-    $('#poll_item_input').keypress(function(event) {
-        if (event.which == 13) {
-            addOption();
-        }
-    });
-    $('#deloptbtn').click(delOptions); 
+  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+  $('.modal-trigger').leanModal();
+
+  $('#poll_item_input').keypress(function(event) {
+    if (event.which == 13) {
+      if ($(this).val() !== "") {
+        addOption();
+      }
+    }
+  });
+  $('#deloptbtn').click(delOptions);
+
+  $('#submit_poll').click(function() {
+
+    if ($('#poll_question').val() !== "" && $('.chips').length!==0) {
+      updatePoll();
+    }
+
+
+
+  });
 });
 
 function addOption() {
-    var optiontext = $('#poll_item_input').val();
-    $('<div class="chip">' + optiontext +
-        '<i class="material-icons">close</i></div>').appendTo(
-        ".pollitems");
-    $('#poll_item_input').val("");
+  var optiontext = $('#poll_item_input').val();
+  $('<div class="chip"><span class="chips">' + optiontext +
+    '</span><i class="material-icons">close</i></div>').appendTo(
+    ".pollitems");
+  $('#poll_item_input').val("");
 }
 
 function delOptions() {
-    $('.pollitems').empty(); // CLEAR ALL POLL ITEMS
-    $('#poll_question').val(""); // CLEAR THE CURRENT POLL QUESTION
+  $('.pollitems').empty(); // CLEAR ALL POLL ITEMS
+  $('#poll_question').val(""); // CLEAR THE CURRENT POLL QUESTION
+}
+
+
+function updatePoll() {
+  // clear and empty array and poll items
+    pollinfo =[];
+    $('#right3_collapsible_body').empty();
+
+
+  $('.right3_collapsible_header').text($('#poll_question').val());
+
+
+  $('.chips').each(function(index, element) {
+
+
+    pollinfo.push($(element).text());
+
+  });
+  console.log(pollinfo);
+
+  for (var x in pollinfo) {
+    $('<li class="collection-item">' + pollinfo[x] + '</li>').appendTo(
+      "#right3_collapsible_body");
+
+
+
+  }
+  // $('#right3_collapsible_body')
+  // <li class="collection-item">yes</li>
+
+
 }
