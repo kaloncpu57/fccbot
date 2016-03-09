43c7792c7f8f3e89d1eca5a7ca2a113fb66ea6a0
SimulatedGREG <simulatedgreg@gmail.com>
2016-03-08 21:54:31 -0600

    final?? and docs

--- README.md
+++ README.md
@@ -7,12 +7,15 @@
  - @Septimus
  - @jackel27
  - @dwd2010
+ - @SimulatedGREG
 
 #### Getting Started
 
-1. ` git clone https://github.com/thejonathangraham/fccbot.git `.
-2. Replace `config/template.json` with `config/default.json` and add the appropriate details. We have added this into the .gitignore file.
-3. Run ` node src/index.js `.
+1. `git clone https://github.com/thejonathangraham/fccbot.git`.
+2. `cd fccbot && npm install`
+3. `npm install -g gulp supervisor`
+4. Replace `config/template.json` with `config/default.json` and add the appropriate details. We have added this into the .gitignore file.
+5. Run `gulp`.
 
 #### Project Structure
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const gulp = require('gulp'),
   autoprefixer = require('gulp-autoprefixer'),
   babel = require('gulp-babel'),
@@ -15,6 +17,11 @@ const gulp = require('gulp'),
  */
 gulp.task('default', ['browser-sync', 'watch']);
 
+/**
+ * Start server using supervisor.
+ * Wait 1 second for server start
+ * then, launch browserSync.
+ */
 gulp.task('browser-sync', () => {
   supervisor('src/app.js', {
     ignore: ['src/dashboard']
@@ -28,6 +35,10 @@ gulp.task('browser-sync', () => {
   }, 1000);
 });
 
+/**
+ * Watch for file changes and reload browser accordingly.
+ * Watches (*.js|*.css|*.sass|*.scss)
+ */
 gulp.task('watch', () => {
   gulp.watch('src/dashboard/views/*.html').on('change', browserSync.reload);
 
@@ -42,6 +53,39 @@ gulp.task('watch', () => {
   ], ['js']).on('change', browserSync.reload);
 });
 
+/**
+ * Generates master vendor.min.css
+ * Add further vendor CSS libraries to the below array
+ * then, run `gulp css:vendor`.
+ */
+gulp.task('css:vendor', () => {
+  return gulp.src([
+    'node_modules/materialize-css/dist/css/materialize.min.css'
+  ]).pipe(cssnano())
+    .pipe(concat('vendor.min.css'))
+    .pipe(gulp.dest('src/dashboard/static'));
+});
+
+
+/**
+ * Generates master vendor.min.js
+ * Add further vendor JS libraries to the below array
+ * then, run `gulp js:vendor`.
+ */
+gulp.task('js:vendor', () => {
+  return gulp.src([
+    'node_modules/jquery/dist/jquery.min.js',
+    'node_modules/materialize-css/dist/js/materialize.min.js'
+  ]).pipe(uglify())
+    .pipe(concat('vendor.min.js'))
+    .pipe(gulp.dest('src/dashboard/static'));
+});
+
+/**
+ * Compiles SASS/SCSS, Autoprefixes, Minifys,
+ * and concat into `styles.min.css`.
+ * This command run on file changes then reloads browser.
+ */
 gulp.task('sass', () => {
   return gulp.src([
     'src/dashboard/static/css/*.css',
@@ -56,6 +100,11 @@ gulp.task('sass', () => {
     .pipe(browserSync.reload({ stream: true }));
 });
 
+/**
+ * Compiles ES2015 using Babel, minifies,
+ * and concat into `main.min.js`.
+ * This command runs on file changes then reloads browser.
+ */
 gulp.task('js', () => {
   return gulp.src([
     'src/dashboard/static/js/*.js'
